4db733b70e27d49b838890583f5a063b46a9431d
Tremayne Johnson <tremaynejohnson@Tremaynes-MacBook-Air.local>
2023-10-05 15:48:29 -0400

    first commity

---
+++ index.js
@@ -0,0 +1,14 @@
+const nums = [3, 5, 7,2,] //this is an array of numbers
+const nums2 = [3, 4, 6, 4, ] // this is also an array on numbers
+
+// console.log(nums[2]) // this will output number 7
+
+let fruits = ['bananas', 'apples', 'kiwi']
+console.log(fruits[1]) //this will output apple
+fruits[0] = 'pineapples' // change value of first item to pineapple'
+
+console.log(fruits[0]) //this will output pineapple
+
+let things = [6, "Todd", false,"false", [2, 5]] //this array has different data types in it
+
+console.log(things[4])
